3ef98f92ca9a8647714360dcea4e2d1c03244fe9
Lautarogalarza <40771990+Lautarogalarza@users.noreply.github.com>
2018-09-03 23:52:35 -0300

    Add files via upload

---
+++ tp_prueba1/math.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ingresarNumero (float* numero)
+{
+
+printf("ingrese un numero: ");
+fflush(stdin);
+scanf("%f",numero);//no nesecita & por que ya le paso la direccion de memoria con el puntero
+
+}
+int division(float numero1, float numero2, float* resultadoDivision)
+{
+     int retorno =-1;
+
+    if(numero2 !=0)
+    {
+
+    *resultadoDivision=numero1/numero2;
+    retorno=0;
+
+    }
+
+    return retorno;
+}
+int suma(float numero1, float numero2, float resultadoSuma)
+{
+    resultadoSuma=numero1+numero2;
+
+    return (int)resultadoSuma;
+}
+int resta(float numero1, float numero2, float resultadoResta)
+{
+    resultadoResta=numero1-numero2;
+
+    return (int)resultadoResta;
+}
+int multiplicacion(float numero1, float numero2, float resultadoMultiplicacion)
+{
+    resultadoMultiplicacion=numero1*numero2;
+
+    return (int)resultadoMultiplicacion;
+}
+
+int factorial(float num)
+{
+    int resultadoFactorial=1;
+    int i;
+
+    if(num>=0)
+
+    for(i=(int)num; i>=1; i--)
+    {
+        resultadoFactorial= resultadoFactorial*i;
+    }
+    return resultadoFactorial;
+
+}
---
+++ tp_prueba1/math.h
@@ -0,0 +1,13 @@
+
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+
+void ingresarNumero(float* numero);
+int division(float numero1, float numero2, float* resultadoDivision);
+int suma(float numero1, float numero2, float resultadoSuma);
+int resta(float numero1, float numero2, float resultadoResta);
+int multiplicacion(float numero1, float numero2, float resultadoMultiplicacion);
+int factorial (float );
+
+
+#endif // MATH_H_INCLUDED
